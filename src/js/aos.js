c2a01c5cbacc911f455e1603e305805f2e175712
Procapi-it <soporte@procapi.com>
2024-07-02 11:22:06 -0500

    modificar carrusel

--- src/js/aos.js
+++ src/js/aos.js
@@ -3,3 +3,14 @@ import AOS from "aos";
 export function aosInit() {
   AOS.init();
 }
+
+// import Swiffy Slider JS
+import { swiffyslider } from 'swiffy-slider'
+window.swiffyslider = swiffyslider;
+
+window.addEventListener("load", () => {
+    window.swiffyslider.init();
+});
+
+// import Swiffy Slider CSS
+import "swiffy-slider/css"
