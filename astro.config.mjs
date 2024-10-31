e65d89a9fd4e85f2bf91c732892694513f9be415
Roberto J. Vargas <rvargas@rv-solutions.net>
2024-10-31 10:46:55 -0500

    Bugfix: Arrelgando analitycs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,11 @@
 import { defineConfig } from 'astro/config';
+
 import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://almacenajes-minidepositos.com', 
-  integrations: [
-    tailwind(),
-    {
-      name: 'google-analytics',
-      hooks: {
-        'astro:config:setup': ({ injectScript }) => {
-          injectScript('page', `
-            <!-- Google tag (gtag.js) -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-W9FEEJYKVT"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.PUBLIC_GA_ID}');
-            </script>
-          `);
-        },
-      },
-    },
-  ],
+  integrations: [tailwind()],
   build: {
     assets: "src",
-  },
+},
 });
